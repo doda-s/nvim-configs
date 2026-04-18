d5d779d0f6255953bbe832a0b5f2246e666e38ff
Dodas <ph005009004@gmail.com>
2026-04-18 11:01:41 -0300

    add some editor configs and andd treesitter plugin configuration

---
+++ lua/config/editor.lua
@@ -0,0 +1,21 @@
+-- EDITOR --
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- SEARCH --
+vim.opt.ignorecase = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- QOL --
+vim.g.mapleader = " ";    -- Set the <leader> in the remaps
+vim.g.maplocalleader = ",";
+vim.opt.clipboard = "unnamedplus"
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+        require 'nvim-treesitter.install'.compilers = { "clang" }
+        require 'nvim-treesitter.install'.install {
+            "lua", "vim", "vimdoc", "query", 
+            "go", "java", "javascript", "c_sharp", 
+            "python", "typescript", "bash", "markdown" 
+        }
+        require 'nvim-treesitter'.setup {
+            ensure_installed = { 
+                "lua", "vim", "vimdoc", "query", 
+                "go", "java", "javascript", "c_sharp", 
+                "python", "typescript", "bash", "markdown" 
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = true,
+            },
+        }
+    end
+}
